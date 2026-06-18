a0f6b821309a702116d0f813c9c54be7528eb8e1
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-06-18 11:10:25 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        port(8080);
+
+        get("/", (req, res) -> "Hello Pavan! DevSecOps Pipeline Working ✅");
+    }
+}
